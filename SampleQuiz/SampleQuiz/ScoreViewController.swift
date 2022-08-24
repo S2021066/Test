2b03b2846ab0f52444a07f43bae18ec00795ad43
s2021066 <s2021066@cco.kanagawa-it.ac.jp>
2022-08-25 01:07:50 +0900

    Xcodeでの開発練習2日目

---
+++ SampleQuiz/SampleQuiz/ScoreViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ScoreViewController.swift
+//  SampleQuiz
+//
+//  Created by KiyoLab on 2022/08/24.
+//
+
+import UIKit
+
+class ScoreViewController: UIViewController {
+    @IBOutlet weak var scoreLabel: UILabel!
+    
+    var correct = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        scoreLabel.text = "\(correct)問正解"
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func toTopButtonAction(_ sender: Any) {
+        self.presentingViewController?.presentingViewController?.dismiss(animated: true)
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
